b736ff3e0a5168ea044364407b97cd8db154394e
gretzke <daniel@gretzke.de>
2024-09-24 02:21:58 +0200

    enable compilation with experimental compilation restrictions
    install with `foundryup --pr 8668`

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-git submodule update --init --recursive --depth 1
-forge clean;
-repos=$(ls src/pkgs/);
-for repo in $repos
-do
-    echo "Starting build of $repo";
-    FOUNDRY_PROFILE=$repo forge build;
-done
